02da83ab686237718d2bfc1af53c1dc6b3fae0a3
Liyan Zhao <return65535@qq.com>
2022-09-17 02:48:47 +0800

    chore: added GPL info

--- Symbol.cpp
+++ Symbol.cpp
@@ -1,6 +1,21 @@
-//
-// Created by 赵李言 on 2022/9/6.
-//
+/*********************************************************************
+Seserot - My toy compiler
+Copyright (C) 2022  zly2006
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*********************************************************************/
+
 #include "Symbol.h"
 
 bool Seserot::isPrivateModifier(Modifiers modifiers) {
