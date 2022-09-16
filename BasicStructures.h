25f5845f1b91c8ec80406d8e6cfa957cc95c9e4d
Liyan Zhao <return65535@qq.com>
2022-09-17 02:52:01 +0800

    fix: assert not found

--- BasicStructures.h
+++ BasicStructures.h
@@ -21,6 +21,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <string>
 #include <utility>
 #include <vector>
+#include <cassert>
 
 namespace Seserot {
     struct SourcePosition {
